603bda5e2344b40c20831a5404df8448c4dd8bd0
Dimitri Korsch <dimitri.korsch@uni-jena.de>
2021-04-25 12:12:42 +0200

    added static folder. Added STATIC_ROOT setting

--- randomizer/settings.py
+++ randomizer/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent#.parent
-
+print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -138,5 +138,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "core/media"
 ]
+
+STATIC_ROOT = os.environ.get("STATIC_ROOT", BASE_DIR / "static")
 
 ANONYMOUS_USER_COOKIE_ID = "anouserID"
